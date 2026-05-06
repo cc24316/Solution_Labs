f8e74ec203529fb4bfc1e236187110588ed63f5e
Isabela Pak Bragagnolo <162496392+cc24316@users.noreply.github.com>
2026-05-06 11:27:28 -0300

    Update Fila.java

--- program/Fila.java
+++ program/Fila.java
@@ -1,1 +1,206 @@
+import java.lang.reflect.*;
 
+public class Fila <X> implements Cloneable
+{
+    private X[]    elemento; // private X[] elemento;
+    private final int   tamanhoInicial;
+    private int         inicio=0, fim=0, qtd=0;
+    private Clonador<X> clonador;
+
+    public Fila (int tamanho) throws Exception
+    {
+        if (tamanho<=0)
+            throw new Exception ("Tamanho invalido");
+
+        this.elemento = (X[]) new Object[tamanho]; //this.elemento=new X [tamanho];
+        this.tamanhoInicial = tamanho;
+
+        this.clonador = new Clonador<X> ();
+    }
+
+    public Fila ()
+    {
+        this.elemento = (X[]) new Object[3];
+        this.tamanhoInicial = 3;
+
+        this.clonador = new Clonador<X> ();
+    }
+
+
+    public void guardeUmItem (X x)// FIFO
+    {
+
+
+        if (x instanceof Cloneable)
+            this.elemento[this.fim]=this.clonador.clone(x);
+        else
+            this.elemento[this.fim]=x;
+
+        this.fim=this.fim==this.elemento.length-1?0:this.fim+1;
+        this.qtd++;
+    }
+
+    public X recupereUmItem () throws Exception // FIFO
+    {
+        if (this.qtd==0) // vazia
+            throw new Exception ("Nada a recuperar");
+
+        X ret=null;
+        if (this.elemento[this.inicio] instanceof Cloneable)
+            ret = this.clonador.clone((X)this.elemento[this.inicio]);
+        else
+            ret = (X)this.elemento[this.inicio];
+
+        return ret;
+    }
+
+    public X removaUmItem()  // FIFO
+    {
+
+        X item = this.elemento[this.inicio];
+        this.elemento[this.inicio] = null;
+        this.inicio = this.inicio == this.elemento.length - 1 ? 0 : this.inicio + 1;
+        this.qtd--;
+
+        return item;                                 // retorna o item removido
+    }
+
+    public int getTamanhoInicial(){
+        return tamanhoInicial;
+    }
+
+
+    public boolean isCheia ()
+    {
+        return this.qtd==this.elemento.length;
+       /*
+        if(this.qtd==this.elemento.length)
+            return true;
+
+        return false;
+        */
+    }
+
+    public boolean isVazia ()
+    {
+        return this.qtd==0;
+       /*
+        if (this.qtd==0)
+            return true;
+
+        return false;
+        */
+    }
+
+    @Override
+    public String toString ()
+    {
+        String ret = this.qtd + " elemento(s)";
+
+        if (this.qtd!=0)
+            ret += ", sendo o primeiro "+this.elemento[this.inicio];
+
+        return ret;
+    }
+
+    @Override
+    public boolean equals (Object obj)
+    {
+        if(obj==this)
+            return true;
+
+        if(obj==null)
+            return false;
+
+        if(obj.getClass()!=this.getClass())
+            return false;
+
+        Fila<X> fil = (Fila<X>) obj;
+
+        if(this.qtd!=fil.qtd)
+            return false;
+        /*
+        if(this.tamanhoInicial!=fil.tamanhoInicial)
+            return false;
+        */
+
+        for(int i=0,
+            atualThis=this.inicio,
+            atualFil=fil.inicio;
+
+            i<this.qtd; // i<fil.qtd
+
+            i++,
+                    atualThis=atualThis==this.elemento.length-1?0:atualThis+1,
+                    atualFil=atualFil==this.elemento.length-1?0:atualFil+1)
+
+            if(!this.elemento[atualThis].equals(fil.elemento[atualFil]))
+                return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode ()
+    {
+        int ret=666/*qualquer positivo*/;
+
+        ret = ret*7/*primo*/ + ((Integer)(this.qtd           )).hashCode();
+        //ret = ret*7/*primo*/ + ((Integer)(this.tamanhoInicial)).hashCode();
+
+        for(int i=0,
+            atual=this.inicio;
+
+            i<this.qtd;
+
+            i++,
+                    atual=atual==this.elemento.length-1?0:atual+1)
+
+            ret = ret*7/*primo*/ + this.elemento[atual].hashCode();
+
+        if (ret<0)
+            ret=-ret;
+
+        return ret;
+    }
+
+    // construtor de copia
+    public Fila (Fila<X> modelo) throws Exception
+    {
+        if(modelo == null)
+            throw new Exception("Modelo ausente");
+
+        this.tamanhoInicial = modelo.tamanhoInicial;
+        this.inicio         = modelo.inicio;
+        this.fim            = modelo.fim;
+        this.qtd            = modelo.qtd;
+        this.clonador       = modelo.clonador;
+
+        this.elemento = (X[]) new Object[modelo.elemento.length];
+
+        for(int i=0,
+            atual=this.inicio;
+
+            i<this.qtd;
+
+            i++,
+                    atual=atual==this.elemento.length-1?0:atual+1)
+
+            this.elemento[atual] = modelo.elemento[atual];
+    }
+
+    @Override
+    public Object clone ()
+    {
+        Fila<X> ret=null;
+
+        try
+        {
+            ret = new Fila<X>(this);
+        }
+        catch(Exception erro)
+        {}
+
+        return ret;
+    }
+}
