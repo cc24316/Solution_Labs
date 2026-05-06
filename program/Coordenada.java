fe17e32e4fce8e37140b4d214ca2fdd113740361
Isabela Pak Bragagnolo <162496392+cc24316@users.noreply.github.com>
2026-05-06 11:26:30 -0300

    Update Coordenada.java

--- program/Coordenada.java
+++ program/Coordenada.java
@@ -1,1 +1,22 @@
+public class Coordenada {
+    private int linha;
+    private int coluna;
 
+    public Coordenada(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + linha + ", " + coluna + ")";
+    }
+}
